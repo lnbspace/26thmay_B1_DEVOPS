5d16e208dc18b7359f3df30c74b02d2ca9707f27
lnbspace <79511584+lnbspace@users.noreply.github.com>
2021-06-22 08:32:35 +0530

    Create while.py

---
+++ while.py
@@ -0,0 +1,11 @@
+import time
+
+while 2 > 1:
+    print("Hello world..")
+    time.sleep(1)
+    print("Welcome to COntianer world...")
+    time.sleep(2)
+    print("___---------------------")
+    print("___---------------------")
+    print("___---------------------")
+    print("___---------------------")
